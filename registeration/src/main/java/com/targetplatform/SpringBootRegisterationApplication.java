54d11214419678a0b5b383978ba9f65fca568ad3
shreya-dotcom <77908208+shreya-dotcom@users.noreply.github.com>
2021-07-13 22:51:28 -0700

    Add files via upload

---
+++ registeration/src/main/java/com/targetplatform/SpringBootRegisterationApplication.java
@@ -0,0 +1,13 @@
+package com.targetplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRegisterationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRegisterationApplication.class, args);
+	}
+
+}
