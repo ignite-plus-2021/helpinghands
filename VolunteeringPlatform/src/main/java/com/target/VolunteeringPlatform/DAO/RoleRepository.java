4638a2ae1c0ba2ceef67f53d44d98916f86c832f
Shraddha Durge <44067306+ShraddhaDurge@users.noreply.github.com>
2021-07-15 20:15:00 +0530

    Add files via upload

---
+++ VolunteeringPlatform/src/main/java/com/target/VolunteeringPlatform/DAO/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.target.VolunteeringPlatform.DAO;
+
+import com.target.VolunteeringPlatform.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("roleRepository")
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Role findByRole(String role);
+
+}
+
