28deb2132ab13b13a91e24038d560c621f00bc73
Kausalya R <68281796+iamk222000@users.noreply.github.com>
2021-07-16 16:55:08 +0530

    Update common.js

--- login/src/components/common.js
+++ login/src/components/common.js
@@ -1,4 +1,4 @@
-export const getUser=()=>{
+/*export const getUser=()=>{
     const userStr=sessionStorage.getItem("user");
     if(userStr) return JSON.parse(userStr);
     else return null;
@@ -13,4 +13,4 @@ export const getUser=()=>{
     export const removeUserSession=()=>{
         sessionStorage.removeItem("token");
         sessionStorage.removeItem("user");
-    }
+    }*/
