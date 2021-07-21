7f45f3aeffa2775615fb808bfa43396662111467
shreya-dotcom <77908208+shreya-dotcom@users.noreply.github.com>
2021-07-21 04:44:48 -0700

    Add files via upload

---
+++ VolunteeringplatformProfile/src/main/java/com/target/Volunteeringplatform/VolunteeringPlatformProfileApplication.java
@@ -0,0 +1,13 @@
+package com.target.Volunteeringplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteeringPlatformProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolunteeringPlatformProfileApplication.class, args);
+	}
+
+}
