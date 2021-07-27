fb1dcf79bf2175d66c8170047d2fb90df8af1c7d
Geethanjali Madhu <78840047+gm1347@users.noreply.github.com>
2021-07-27 12:00:00 +0530

    Add files via upload

---
+++ Past-Events Frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import PastEvents from './Components/PastEvents';
+
+function App() {
+  return (
+    <div className="App">
+      <PastEvents />
+    </div>
+  );
+}
+
+export default App;
