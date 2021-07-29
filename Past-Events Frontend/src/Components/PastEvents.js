5c9f86a88e64568cf39d7fa1f6667af8bee1bb2e
Geethanjali Madhu <78840047+gm1347@users.noreply.github.com>
2021-07-29 16:53:42 +0530

    Update PastEvents.js

--- Past-Events Frontend/src/Components/PastEvents.js
+++ Past-Events Frontend/src/Components/PastEvents.js
@@ -1,86 +1,98 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {Grid, Paper, Button, Typography,  Step, Stepper} from '@material-ui/core';
 
-import {Grid,  Paper, Button, IconButton, Dialog, Typography} from '@material-ui/core';
+//import {KeyboardArrowLeft, KeyboardRight} from '@material-ui/icons';
 
-//import SimpleImageSlider from 'react-simple-image-slider';
-
-import {Formik, Form, Field,} from 'formik';
+import {Formik, Form } from 'formik';
 import logo from './HH-logo.jpg';
 import wkndevnt1 from './img1.jpg';
 import wkndevnt2 from './img2.jpg';
 import axios from 'axios';
-//import styled from 'styled-components';
 
-/*const images = [
-    { url: "./img1.jpg" },
-    { url: "./istockphoto-518197647-1024x1024.jpg" },
-    { url: "./20210605_155233.jpeg" },
-    
-  ];*/
 
 const PastEvents = (props) => {
-    const paperStyle = {padding:'10px 10px',  height:'90%', width:'90%', border:''}
+    const paperStyle = {padding:'10px 10px',  border:''}
     const headStyle = {margin:'0', fontFamily:'serif', color:'blue'}
     const btnStyle = {margin:'8px 0' }
     const logoStyle = {height:98, width:128}
     const imgStyle = {height:'90%', width:'90%'}
+    //const stepperStyle = {}
 
-    const dialogStyle = {margin:1, padding:1}
+    //var maxSteps = (3);
 
-    /*const DialogContent = withStyles((theme) => {
-        const dialogContentStyle = { padding: theme.spacing(2) }
-    })(MuiDialogContent);
-
-    const DialogActions = withStyles((theme) => {
-        const dialogActionsStyle = { margin: 0, padding: theme.spacing(1) }
-    })(MuiDialogActions);*/
-
-    const handleClick = () => {
-        setOpen(true);
+    /*const tutorialSteps = [
+        {label:'1', imgPath:'./img1.jpg',},
+        {label:'2', imgPath:'./Screenshot 2021-07-27 152014.jpg',},
+        {label:'3', imgPath:'./old-age-home-batu-pahat-cartoon.jpg',},
+    ]*/
+    /*const [activeStep, setActiveStep] = React.useState(0);
+    const handleNext = () => {
+        if(activeStep < 2)
+            setActiveStep((currentStep) => currentStep + 1);
     };
-
+    const handleBack = () => {
+        if(activeStep !== -1)
+            setActiveStep((currentStep) => currentStep - 1);
+    };*/
 
     /*const handleChange = () => {
         props.history.push('/home)')
     }*/
 
-    /*var pastEvents
-    console.log(pastEvents)
-    axios.post("localhost:8080/account/event/eventType/pastEvents", pastEvents)
-    .then((response) => {
-        console.log(response.data);
-    }).catch((error) => {
-        if(error.response.status===400 || error.response.status===401) {
-            console.log(error.response.data.message)
-        }
-        else {
-            console.log("Oop! Something went wrong")
-        }
-    })*/
+    const [pevent, setPevent] = useState([])
+    const event = "Past event"
+    useEffect(() => {
+        axios.get('http://localhost:8081/account/events/getEventsList/false/Weekend event')
+        //('http://localhost:8081/account/events/getEvents/'.concat('/isFutureEvent').concat('past'))
+        //(`http://localhost:8081/account/events/getEventsList/isFutureEvent${past}/eventTypes${event}`)
+        .then(response => {
+                console.log(response)
+                setPevent(response.data)
+        })
+        .catch(error => {
+            if(error.response.status===400 || error.response.status===401) {
+                console.log(error.response.data.message)
+            }
+            else {
+                console.log("Oop! Something went wrong")
+            }
+        })
+    },[event])
 
+    
     return(
         <Grid padding='20px 20px'>
-            <Paper style={paperStyle} elevation={0}>
+            <Paper style={paperStyle} elevation={1}>
                 <Grid align='center'>
                     <img src={logo} style={logoStyle} alt="Logo" />
                 </Grid>
-
-                <Grid item xs={6} sm={3}>
+                <Grid item xs={12} align='left'>
                 <Typography>
-                    <h2 style={headStyle}>Past Events </h2>
+                    <h2 style={headStyle}>Past Events</h2>
                 </Typography>
                 </Grid>
                 <br></br>
                 <br></br>
-                <div style={{columnCount:2}}>
+                <div style={{columnCount:2}} >
                     <Grid item xs={12} sm={6}>
-                        <Formik>
+                        <Formik >
                             {(props) => (
-                                <Form>
-                                    <img src={wkndevnt1} style={imgStyle} onClick={handleClick} alt="Restore Our Earth" />
-
-
-
+                                <Form disabled={true} >
+                                   
+                                    <Stepper  variant="text"  >
+                                        <Step>
+                                            <img src={'./img1.jpg'} alt="1" />
+                                        </Step>
+                                        <Step>
+                                            <img src={'./Screenshot 2021-07-27 152014.jpg'} alt="2" />
+                                        </Step>
+                                        <Step>
+                                            <img src={'./old-age-home-batu-pahat-cartoon.jpg'} alt="3" />
+                                        </Step>
+                                    </Stepper>
+                                    <Button size="large" color="solid black" onClick > 《  </Button>
+                                    <Button size="large" color="solid black" onClick >  》 </Button>  
+                                    
                                 </Form>
                             )}
                         </Formik>
@@ -89,24 +101,32 @@ const PastEvents = (props) => {
                     <Grid item xs={12} sm={6}>
                         <Formik>
                             {(props) => (
-                                <Form>
-                                    <Field as={Date} />
-                                </Form>
+                                pevent.map(post => (
+                                    <p key={post.id}>
+                                        Event {post.event_id}: {post.name}
+                                        <br></br>
+                                        Date: {post.start_time} - {post.end_time}
+                                        <br></br>
+                                        Venue: {post.venue}
+                                        <br></br>
+                                        About: {post.description}
+                                    </p>
+                                ))
                             )}
                         </Formik>
                     </Grid>
                 </div>
-                
                 <br></br>
                 <br></br>
                 <br></br>
-                
                 <div style={{columnCount:2}}>
                     <Grid item xs={12} sm={6}>
                         <Formik>
                             {(props) => (
                                 <Form>
-                                    <img src={wkndevnt2} style={imgStyle} onClick={handleClick} alt="Animal Shelter" />
+                                    
+                                    <img src={wkndevnt2} style={imgStyle} alt="Green Earth" />
+                                    
                                 </Form>
                             )}
                         </Formik>
@@ -115,23 +135,30 @@ const PastEvents = (props) => {
                     <Grid item xs={12} sm={6}>
                         <Formik>
                             {(props) => (
-                                <Form>
-                                    <Field as={Date} />
-                                </Form>
+                                pevent.map(post => (
+                                    <p key={post.id}>
+                                        Event {post.event_id}: {post.name}
+                                        <br></br>
+                                        Date: {post.start_time} - {post.end_time}
+                                        <br></br>
+                                        Venue: {post.venue}
+                                        <br></br>
+                                        About: {post.description}
+                                    </p>
+                                ))
                             )}
                         </Formik>
                     </Grid>
                 </div>
-                
                 <br></br>
                 <br></br>
                 <br></br>
                 <Grid align='right' item xs={6} sm={3}>
+                    <br></br>
                     <Button style={btnStyle} color='primary' variant='contained' >Go To Home Page</Button>
                 </Grid>
             </Paper>
-        </Grid>
-            
+        </Grid>    
     )
 }
 
