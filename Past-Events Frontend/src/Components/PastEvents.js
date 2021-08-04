f972acddc124b47b571dd940fccd177d039fb250
Geethanjali Madhu <78840047+gm1347@users.noreply.github.com>
2021-08-04 14:46:16 +0530

    Update PastEvents.js

--- Past-Events Frontend/src/Components/PastEvents.js
+++ Past-Events Frontend/src/Components/PastEvents.js
@@ -1,19 +1,33 @@
 import React, {useEffect, useState} from 'react';
-import {Grid, Paper, Button, Typography, Tooltip, Table, TableRow, TableCell } from '@material-ui/core';
-import {Formik, Form} from 'formik';
+import {Grid, Box, Card, CardContent, Button, Typography, Tooltip, makeStyles} from '@material-ui/core';
 import logo from './HH-logo.jpg';
 import wkndevnt1 from './img1.jpg';
-import wkndevnt2 from './img2.jpg';
+//import wkndevnt2 from './img2.jpg';
 import moment from 'moment';
 import axios from 'axios';
 
+const useStyles = makeStyles({
+    card:{
+        backgroundColor:"#D6EAF8",
+        '&:hover':{
+            backgroundColor:"#EBF5FB",
+        }
+    }
+    ,button:{
+        color:"primary",
+        '&:hover':{
+            backgroundColor:"#2471A3",
+        },
+        marginTop:"8px"
+    }
+    });
+
 const PastEvents = (props) => {
-    const paperStyle = {padding:'10px 25px', margin:'25px auto', border:'none'}
+    const gridStyle={margin:'3px auto', padding:'5px auto'}
     const headStyle = {margin:'0', fontFamily:'serif', color:'blue'}
-    const btnStyle = {margin:'8px 0' }
+    const btnStyle = {margin:'8px 0'}
     const logoStyle = {height:98, width:128}
-    const imgStyle = {height:'100px', width:'180px'}
-    const tableRowStyle = {border:'white'}
+    
     //const stepperStyle = {}
 
 
@@ -28,8 +42,9 @@ const PastEvents = (props) => {
         //('http://localhost:8081/account/events/getEvents/'.concat('/isFutureEvent').concat('past'))
         //(`http://localhost:8081/account/events/getEventsList/isFutureEvent${past}/eventTypes${event}`)
         .then(response => {
-                console.log(response)
-                setPevent(response.data)
+            console.log(response)
+            console.log(response.data[0].event_id)
+            setPevent(response.data)
         })
         .catch(error => {
             if(error.response.status===400 || error.response.status===401) {
@@ -41,88 +56,64 @@ const PastEvents = (props) => {
         })
     },[event])
 
-    //var date = '20010704T120854'
-    
+    const classes = useStyles();
     return(
-        <Grid padding='10px 10px'>
-            <Paper style={paperStyle} elevation={0}>
-                <Grid align='center'>
-                    <img src={logo} style={logoStyle} alt="Logo" />
+        <Box>
+            <Box mt={1} mb={3} align="center">
+                <img src={logo} style={logoStyle} alt="Logo" />
+            </Box>
+
+            <Box align="center">
+                <Typography variant='h4' style={headStyle}>
+                    <b> Past Events</b>
+                </Typography>
+            </Box>
+
+            <Box m={5}>
+                <Grid container spacing={6}>
+                    {pevent.map((post)=>(
+                        <Grid item xs={12} sm={6} md={6}>
+                            <Card style={{minwidth:200}} className={classes.card}>
+                                <CardContent>
+                                    <Grid container spacing={5}>
+                                        <Grid item xs={6} style={gridStyle}>
+                                            <Tooltip title={post.name}>
+                                                <img src={wkndevnt1} height='100%' width='100%' alt="Old Age Home" />
+                                            </Tooltip>
+                                        </Grid>
+                                        <Grid item xs={6} style={gridStyle}>
+                                            <p align="left">
+                                                <br></br>
+                                                <b>{post.name}</b>
+                                                <br></br>
+                                                Event ID: {post.event_id}
+                                                <br></br>
+                                                Venue: {post.venue}
+                                                <br></br>
+                                                Date: {moment(post.start_time).format('MMMM Do YYYY')}
+                                                <br></br>
+                                                Start-Time: {moment(post.start_time).format('h:mm a')}
+                                                <br></br>
+                                                End-Time: {moment(post.end_time).format('h:mm a')}
+                                                <br></br>
+                                            </p>
+                                            
+                                        </Grid>
+                                    </Grid>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    ))}
                 </Grid>
-                <Grid item xs={12} align='left'>
-                    <Typography>
-                        <h2 style={headStyle}>Past Events</h2>
-                    </Typography>
-                </Grid>
-                
-                <div  >
-                    <Grid>
-                        <Table>
-                            <TableRow style={tableRowStyle}>
-                                <TableCell >
-                                    <Tooltip title="Old Age Home">
-                                        <img src={wkndevnt1} style={imgStyle} alt="Old Age Home" />
-                                    </Tooltip> 
-                                </TableCell>
-                                <TableCell align='left'>            
-                                    <Formik>
-                                        {(props) => (
-                                            pevent.map(post => (
-                                                <p key={post.id=0}>
-                                                    <ul align='left'>
-                                                        <b>{post.name}</b>
-                                                        <br></br>
-                                                        Event ID: {post.event_id}
-                                                        <br></br>
-                                                        Date:   {moment(post.start_time).format('LLLL')}
-                                                        <br></br>
-                                                        Venue:  {post.venue}
-                                                        <br></br>
-                                                        About:  {post.description}
-                                                    </ul>
-                                                </p>
-                                            ))
-                                        )}
-                                    </Formik>
-                                </TableCell>
-                            </TableRow>
-                            <TableRow style={tableRowStyle}>
-                                <TableCell >
-                                    <Tooltip title="Green Earth">
-                                        <img src={wkndevnt2} style={imgStyle} alt="Green Earth" />
-                                    </Tooltip>
-                                </TableCell>
-                                <TableCell align='left'>
-                                    <Formik>
-                                        {(props) => (
-                                            pevent.map(post => (
-                                                <p key={post.id=1}>
-                                                    <ul align='left'>
-                                                        <b>{post.name}</b>
-                                                        <br></br>
-                                                        Event ID: {post.event_id}
-                                                        <br></br>
-                                                        Date:   {moment(post.start_time).format('LLLL')}
-                                                        <br></br>
-                                                        Venue:  {post.venue}
-                                                        <br></br>
-                                                        About:  {post.description}
-                                                    </ul>
-                                                </p>
-                                            ))
-                                        )}
-                                    </Formik>
-                                </TableCell>
-                            </TableRow>
-                        </Table>
-                    </Grid>
-                </div>
                 <br></br>
-                <Grid align='right'>
-                    <Button style={btnStyle} color='primary' variant='contained' >Go To Home Page</Button>
+                <br></br>
+                <br></br>
+                <Grid align="right">
+                    <Button style={btnStyle} color='primary' variant='contained'>Go To Home Page</Button>
                 </Grid>
-            </Paper>
-        </Grid>    
+            </Box>
+        </Box>
+
     )
 }
 
