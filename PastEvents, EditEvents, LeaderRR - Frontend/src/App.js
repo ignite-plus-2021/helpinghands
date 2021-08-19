2175efae343e89d6665923670f22d939a4834e80
Geethanjali Madhu <78840047+gm1347@users.noreply.github.com>
2021-08-20 01:53:12 +0530

    Update and rename Past-Events Frontend/src/App.js to PastEvents, EditEvents, LeaderRR - Frontend/src/App.js

---
+++ PastEvents, EditEvents, LeaderRR - Frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import './App.css';
+import PastEvents from './Components/PastEvents';
+import EditEvents from './Components/EditEvents';
+import LeaderRR from './Components/LeaderRR';
+//import home from './Components/home';
+
+function App() {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path='/pastevents' component={PastEvents} />
+		            <Route exact path='/editevents' component={EditEvents} />
+                <Route exact path='/leaderrr' component={LeaderRR} />
+            </Switch>
+        </Router>
+    );
+}
+
+export default App;
